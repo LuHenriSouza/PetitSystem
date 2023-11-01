8293bfb13a970248acf46443d9db26bca2229157
luizhapsouza <luizhapsouza@gmail.com>
2023-11-01 15:39:54 -0300

    Init Shop Page (Need fix the loading animations)

--- app/Livewire/SalePage.php
+++ app/Livewire/SalePage.php
@@ -2,10 +2,78 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class SalePage extends Component
 {
+    // EFETUAR VENDA
+    public $searchTerm = '';
+    public $searchResults = [];
+    public $totalValue;
+
+    public function atualizarResultados()
+    {
+        // Realize a pesquisa no banco de dados
+        $result = Product::where('prod_code', trim($this->searchTerm))->first();
+
+        // Adicione o resultado à matriz de resultados
+        if ($result) {
+            $productId = $result->prod_id;
+
+            // Verifique se o produto já existe na coleção
+            if (isset($this->searchResults[$productId])) {
+                // Produto já existe, aumente o contador
+                $this->addProd($productId);
+            } else {
+                // Produto é novo, adicione à coleção
+                $this->searchResults[$productId] = [
+                    'product' => $result,
+                    'count' => 1,
+                ];
+            }
+
+            $this->attTotalValue($productId);
+        } else {
+            session()->flash('NotFound', 'Produto não encontrado!');
+        }
+
+        // Limpe o campo de pesquisa
+        $this->searchTerm = '';
+    }
+
+    public function addProd($productId)
+    {
+        $this->searchResults[$productId]['count']++;
+        $this->attTotalValue($productId);
+    }
+
+    public function minusProd($productId)
+    {
+        if ($this->searchResults[$productId]['count'] <= 1) {
+            unset($this->searchResults[$productId]);
+        } else {
+            $this->searchResults[$productId]['count']--;
+        }
+
+        $this->attTotalValue($productId);
+    }
+
+    public function attTotalValue()
+    {
+        $total = 0;
+
+        foreach ($this->searchResults as $productId => $item) {
+            $product = $item['product'];
+            $count = $item['count'];
+
+            $total += $count * $product->prod_price;
+        }
+
+        $this->totalValue = $total;
+    }
+
+
     public function render()
     {
         return view('livewire.sale-page');
--- resources/views/livewire/sale-page.blade.php
+++ resources/views/livewire/sale-page.blade.php
@@ -1,26 +1,71 @@
 <div class="row">
     <div class="col-md-5">
         <div class="card shadow mb-4">
+            @if (session('NotFound'))
+                <div class="alert alert-danger d-flex align-items-center alert-dismissible fade show" role="alert">
+                    <i class="fa-solid fa-xmark"></i>
+                    <div class="ms-3">{{ session('NotFound') }}</div>
+                    <button type="button" class="btn-close pb-3" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
             <div class="card-header py-3">
-                <input type="text" name="search" id="search" class="form-control">
+                <input type="text" name="search" id="search" class="form-control" wire:model="searchTerm"
+                    wire:keydown.enter="atualizarResultados" autocomplete="off">
             </div>
             <div class="card-body">
                 <!-- Corpo -->
-                <div class="table-responsive" style="max-height: 300px;">
+                <div class="table-responsive" style="max-height: 1000px;">
                     <table class="table table-striped">
                         <thead>
                             <tr>
                                 <th scope="col">Nome</th>
                                 <th scope="col">Preço</th>
+                                <th scope="col">Quantidade</th>
                             </tr>
                         </thead>
-                        <tbody class="table-group-divider">
-                            <tr>
-                                <td class="align-middle">Luiz askldjaskldjaskljdasjdkjaskldasjasdas</td>
-                                <td class="align-middle">R$32,40</td>
-                            </tr>
+                        <tbody class="table-group-divider" wire:loading.remove wire:target="atualizarResultados">
+                            @if (!empty($searchResults))
+                                @foreach ($searchResults as $item)
+                                    <tr>
+                                        <td class="align-middle">{{ $item['product']->prod_name }}</td>
+                                        <td class="align-middle">{{ $item['product']->prod_price }}</td>
+                                        <td class="align-middle" wire:loading.remove>
+                                            @if ($item['count'] == 1)
+                                                <i class="fa-solid fa-trash-can me-2" style="cursor:pointer;"
+                                                    wire:click="minusProd({{ $item['product']->prod_id }})"></i>
+                                            @else
+                                                <i class="fa-solid fa-caret-left me-2" style="cursor:pointer;"
+                                                    wire:click="minusProd({{ $item['product']->prod_id }})"></i>
+                                            @endif
+                                            <span>{{ $item['count'] }}</span>
+                                            <i class="fa-solid fa-caret-right ms-2" style="cursor:pointer;"
+                                                wire:click="addProd({{ $item['product']->prod_id }})"></i>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endif
                         </tbody>
+
                     </table>
+                </div>
+                @if (!empty($searchResults))
+                    <div class="row">
+                        <div class="col-4 pt-2">
+                            <button class="btn btn-dark ms-2" wire:click="Finish">
+                                <i class="fa-solid fa-cart-plus"></i>
+                                Finalizar
+                            </button>
+                        </div>
+                        <div class="col-4"></div>
+                        <p class="col-3 pt-2"><strong>TOTAL: R$ {{ $totalValue }}</strong></p>
+                    </div>
+                @endif
+                <div wire:loading wire:target="atualizarResultados" class="mx-auto mb-2">
+                    <div class="loading">
+                        <div class="dot"></div>
+                        <div class="dot"></div>
+                        <div class="dot"></div>
+                    </div>
                 </div>
                 <!-- /Corpo -->
 
